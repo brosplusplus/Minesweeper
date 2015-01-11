dcbdd0ca220a866abe3308f2bd0d7521aa985ef9
Borja <borja.arias.upc@gmail.com>
2015-01-11 19:51:46 +0100

    Mogut de package

---
+++ src/com/bros/minesweeper/domain/controller/JugarPartidaUseCaseController.java
@@ -0,0 +1,19 @@
+package com.bros.minesweeper.domain.controller;
+
+import java.util.ArrayList;
+
+public class JugarPartidaUseCaseController {
+
+	/**
+	 * Association attributes
+	 */
+	
+	/**
+	 * Methods
+	 */
+	
+	public void FerAutenticacio(String userN,String passwd)
+	{
+	
+	}
+}
