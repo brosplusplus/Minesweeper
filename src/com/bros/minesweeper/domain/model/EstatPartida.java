d191f6e01d1f4cfb83d03f2aa316d9dfd5960545
Borja <borja.arias.upc@gmail.com>
2015-01-11 19:35:07 +0100

    puta linea en blanc

--- src/com/bros/minesweeper/domain/model/EstatPartida.java
+++ src/com/bros/minesweeper/domain/model/EstatPartida.java
@@ -1,9 +1,5 @@
 package com.bros.minesweeper.domain.model;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 6c634b1123f49d0ea9153b37cfd473ae6673ba14
 public class EstatPartida {
 	public Boolean guanyada;
 	public Boolean acabada;
