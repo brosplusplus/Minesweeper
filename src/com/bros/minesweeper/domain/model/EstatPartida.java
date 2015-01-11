a324b92cded6daaf7b614e569464afaefa80e40e
Borja <borja.arias.upc@gmail.com>
2015-01-11 19:32:28 +0100

    Cosas

--- src/com/bros/minesweeper/domain/model/EstatPartida.java
+++ src/com/bros/minesweeper/domain/model/EstatPartida.java
@@ -1,11 +1,8 @@
 package com.bros.minesweeper.domain.model;
 
-import javax.persistence.Entity;
 
-@Entity
 public class EstatPartida {
 	public Boolean guanyada;
 	public Boolean acabada;
 	public Integer puntuacio;
-	//TODO revisar si faltan cosas, yo (Borja) creo que si
 }
