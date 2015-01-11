f322be62b7421c9fcda2cf20ad3680385e329702
Borja <borja.arias.upc@gmail.com>
2015-01-11 16:59:28 +0100

    Hiberanet Administrador UsuariRegistrat Jugador i Test

--- src/com/bros/minesweeper/domain/model/UsuariRegistrat.java
+++ src/com/bros/minesweeper/domain/model/UsuariRegistrat.java
@@ -1,14 +1,21 @@
 package com.bros.minesweeper.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="UsuariRegistrat")
+@Table(name="USUARIREGISTRAT")
+@Inheritance(strategy=InheritanceType.JOINED)
 public abstract class UsuariRegistrat {
+	@Column(name="nom")
 	private String nom;
+	@Column(name="cognom")
 	private String cognom;
+	@Column(name="pwd")
 	private String pwd;
 	@Id private String username;
 	
