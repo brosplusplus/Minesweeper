a22240e8076ae20b9b26079d5758b6c743d7fc00
Axelred <alex.martinez.romero@gmail.com>
2015-01-11 19:31:15 +0100

    Començada la classe JugarPartidaUseCaseController i afegit TestPartida

--- src/com/bros/minesweeper/domain/model/Partida.java
+++ src/com/bros/minesweeper/domain/model/Partida.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -38,6 +39,7 @@ public class Partida {
 	
 	private EstrategiaPuntuacio estrategia;
 	
+	@OneToMany
 	private ArrayList<ArrayList<Casella>> taulell;
 
 	@Id
