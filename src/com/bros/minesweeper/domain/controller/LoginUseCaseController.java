5592f444d829b613ea9eef475c1528ccd6f95ae3
Axelred <alex.martinez.romero@gmail.com>
2015-01-12 11:35:13 +0100

    Classe LoginUSC acabada

--- src/com/bros/minesweeper/domain/controller/LoginUseCaseController.java
+++ src/com/bros/minesweeper/domain/controller/LoginUseCaseController.java
@@ -1,5 +1,19 @@
 package com.bros.minesweeper.domain.controller;
 
+import com.bros.minesweeper.domain.datainterface.CtrlUsuariRegistrat;
+import com.bros.minesweeper.domain.factory.FactoriaControladors;
+import com.bros.minesweeper.domain.model.UsuariRegistrat;
+
 public class LoginUseCaseController {
+	
+	public Boolean login (String userN, String passwd) throws Exception {
+		FactoriaControladors FC = new FactoriaControladors();
+		CtrlUsuariRegistrat CU = FC.getCtrlUsuari();
+		UsuariRegistrat u = CU.get(userN);
+
+		if(passwd != u.getPwd()) throw new Exception("Password Incorrecta");
+
+		return true;
+	}
 
 }
--- src/com/bros/minesweeper/domain/factory/FactoriaControladors.java
+++ src/com/bros/minesweeper/domain/factory/FactoriaControladors.java
@@ -1,5 +1,12 @@
 package com.bros.minesweeper.domain.factory;
 
+import com.bros.minesweeper.domain.datainterface.CtrlUsuariRegistrat;
+
 public class FactoriaControladors {
+
+	public CtrlUsuariRegistrat getCtrlUsuari() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
