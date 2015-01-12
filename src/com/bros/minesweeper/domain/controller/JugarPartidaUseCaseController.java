425c2be95b3c52ae9ce78f6ee2ac23915e9d7bb4
Axelred <alex.martinez.romero@gmail.com>
2015-01-12 10:39:52 +0100

    Starting with controllers

--- src/com/bros/minesweeper/domain/controller/JugarPartidaUseCaseController.java
+++ src/com/bros/minesweeper/domain/controller/JugarPartidaUseCaseController.java
@@ -2,18 +2,34 @@ package com.bros.minesweeper.domain.controller;
 
 import java.util.ArrayList;
 
+import javassist.util.proxy.FactoryHelper;
+
+import com.bros.minesweeper.domain.model.Partida;
+
 public class JugarPartidaUseCaseController {
 
 	/**
 	 * Association attributes
 	 */
+	private Partida partida;
+	
+	public Partida getPartida() {
+		return partida;
+	}
+
+	public void setPartida(Partida partida) {
+		this.partida = partida;
+	}
 	
 	/**
 	 * Methods
 	 */
 	
-	public void FerAutenticacio(String userN,String passwd)
-	{
-	
+	public void FerAutenticació(String userN,String passwd) {
+		
+	}
+
+	public void obtenirNivells() {//ArrayList<Nivell> 
+		
 	}
 }
